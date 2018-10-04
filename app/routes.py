c296b8e08dc2e2f6db1d8c913bf6881b24cad94a
TonyTheTaiga <tonythetaiga@gmail.com>
2018-10-03 20:48:23 -0700

    format update

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
-@app.route('/picture')
+@app.route("/")
+@app.route("/index")
+def index():
+
+    return render_template("index.html")
+
+
+@app.route("/picture")
 def picture():
-    return render_template('gallery.html')
+
+    return render_template("gallery.html")
+
