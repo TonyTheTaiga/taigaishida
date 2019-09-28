271881cbd04058ad160dc5efe4cb95cb1a36ebcd
tonythetaiga <taigaishida.dev@gmail.com>
2019-09-27 20:47:28 -0700

    pulled changes from develop

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,13 @@ from app import website
 from flask import render_template
 from app.forms import ContactForm
 
+
 @website.route("/")
 @website.route("/index")
 def index():
     form = ContactForm()
     return render_template("index.html")
+
 
 @website.route("/picture")
 def picture():
