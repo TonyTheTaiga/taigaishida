bab6840c01da05796d7a483a29981afc80b1344d
Taiga Ishida <tonythetaiga@gmail.com>
2022-10-10 15:45:21 -0400

    b

--- taigaishida/__init__.py
+++ taigaishida/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
 from config import Config
-from taigaishida import root
+from taigaishida import taiga
 
 
 def create_app():
     site = Flask(__name__)
     site.config.from_object(Config)
 
-    site.register_blueprint(root.bp)
+    site.register_blueprint(taiga.bp)
 
     return site
