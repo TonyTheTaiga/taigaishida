57f543a977694818a75b0a3892af89545e37ba20
Taiga Ishida <tonythetaiga@gmail.com>
2022-10-10 22:02:00 -0400

    done

--- taigaishida/taiga.py
+++ taigaishida/taiga.py
@@ -1,12 +1,24 @@
-from flask import Blueprint, render_template, request
+from pathlib import Path
+import json
+
+from flask import Blueprint, render_template
+from pydantic import BaseModel
 
 bp = Blueprint("taiga", __name__, url_prefix="/")
+
+
+class MetaData(BaseModel):
+    gallery: list
+
+
+with open(Path(__file__).parent.resolve() / "static" / "metadata.json", "r") as fp:
+    metadata = MetaData(**json.loads(fp.read()))
 
 
 @bp.route("")
 @bp.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", gallery=metadata.gallery)
 
 
 @bp.route("/about")
