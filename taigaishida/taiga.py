0c1e52b0c0fd72572f6f311703682f3ab7503360
Taiga Ishida <tonythetaiga@gmail.com>
2023-05-22 19:11:45 +0900

    add basic functionality to upload iamges

--- taigaishida/taiga.py
+++ taigaishida/taiga.py
@@ -2,14 +2,16 @@ from typing import List, Optional
 from pathlib import Path
 import os
 import logging
+import io
+from uuid import uuid4
 
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from google.cloud import storage
-
+from PIL import Image
 
 IMAGE_BUCKET = "taiga-ishida-public"
 IMAGE_PREFIX = "webp_images"
@@ -26,7 +28,8 @@ app.mount(
     name="static",
 )
 templates = Jinja2Templates(directory=Path(__file__).parent.resolve() / "templates")
-client = storage.Client.create_anonymous_client()
+
+client = storage.Client()
 
 
 class GalleryItem(BaseModel):
@@ -59,3 +62,29 @@ async def gallery(request: Request, gallery_items=Depends(get_gallery_items)):
 @app.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
+
+
+@app.post("/upload-image")
+async def upload_image(image: UploadFile = File(...)):
+    # Create a PIL Image object
+    image_pil = Image.open(image.file)
+
+    # Convert image to WebP format in memory
+    byte_arr = io.BytesIO()
+    image_pil.save(byte_arr, format="WEBP")
+    byte_arr.seek(0)  # Move the cursor back to the beginning of the file
+
+    # Generate a new filename
+    filename = f"{IMAGE_PREFIX}/{uuid4()}.webp"
+
+    blob = storage.Blob(filename, client.bucket(IMAGE_BUCKET))
+
+    # Upload the image to GCS
+    blob.upload_from_file(byte_arr, content_type="image/webp")
+
+    return {"message": "Image uploaded successfully", "filename": filename}
+
+
+@app.get("/upload", response_class=HTMLResponse)
+async def upload(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
