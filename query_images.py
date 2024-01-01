2e5045bcf74219e3fea189e9870b62ce74343a9a
Taiga Ishida <tonythetaiga@gmail.com>
2024-01-01 13:18:08 -0500

    pop up style

--- query_images.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import datastore
-
-client = datastore.Client(database="")
-query = client.query(kind="Image")
-rows = list(query.fetch())
-# print(len(rows))
-for row in rows:
-    print(row["latlong"])
-    break
