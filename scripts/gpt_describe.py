8df829d1eddcc5c55854d24ea8cc3f73c6dbdadd
Taiga Ishida <tonythetaiga@gmail.com>
2024-04-25 16:46:17 -0400

    update

--- scripts/gpt_describe.py
+++ scripts/gpt_describe.py
@@ -12,11 +12,13 @@ def b64_encode_image(image: np.ndarray) -> bytes:
     im_bytes = im_arr.tobytes()
     return base64.b64encode(im_bytes)
 
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
-def create_image_url(image):
-    pass
 
-
+format_image = lambda base64_image: f"data:image/webp;base64,{base64_image}"
+image = encode_image('/Users/taiga/Downloads/lifeguard.webp')
 response = client.chat.completions.create(
     model="gpt-4-turbo",
     messages=[
@@ -27,7 +29,8 @@ response = client.chat.completions.create(
                 {
                     "type": "image_url",
                     "image_url": {
-                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                        # "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                        "url": format_image(image),
                     },
                 },
             ],
